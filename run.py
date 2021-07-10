c3d29307da55b9dda17a3ce944b78f8739f28fe1
berkaycubuk <berkaycubuk@gmail.com>
2021-07-10 10:41:29 +0300

    data types fixed

--- run.py
+++ run.py
@@ -5,11 +5,10 @@ wb = load_workbook(filename = '20210624.xlsx')
 sheet = wb.active
 
 master = {}
-master['cities'] = {}
+master['cities'] = []
 data = master['cities']
 
-for row in sheet.iter_rows(min_row=2, max_col=4, values_only=True):
-    ### data.add((row[0], row[1], row[3]))
+for row in sheet.iter_rows(min_row=2, max_row=50, max_col=4, values_only=True):
     city = row[0]
     city = city.rstrip();
 
@@ -19,17 +18,37 @@ for row in sheet.iter_rows(min_row=2, max_col=4, values_only=True):
     neighborhood = row[3]
     neighborhood = neighborhood.rstrip();
     
-    if city not in data.keys():
-        data[city] = {
-            'districts': {}
-        }
+    foundCity = False
+    foundDistrict = False
+    foundNeighborhood = False
+    
+    for x in data:
+        if x['name'] == city:
+            for y in x['districts']:
+                if y['name'] == district:
+                    for z in y['neighborhoods']:
+                        if z == neighborhood:
+                            
+                            foundNeighborhood = True
 
-    if district not in data[city]['districts'].keys():
-        data[city]['districts'][district] = {
-            'neighborhoods': []
-        }
+                    if not foundNeighborhood:
+                        y['neighborhoods'].append(neighborhood)
+                    
+                    foundDistrict = True
 
-    data[city]['districts'][district]['neighborhoods'].append(neighborhood)
+            if not foundDistrict:
+                x['districts'].append({
+                    'name': district,
+                    'neighborhoods': []
+                })
+            
+            foundCity = True
+
+    if not foundCity:
+        data.append({
+            'name': city,
+            'districts': []
+        })
 
 with open('output.json', 'w') as outfile:
     json.dump(master, outfile)
